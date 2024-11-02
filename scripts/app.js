e5bf1618a2acd8022759b1a67ac3c1e108206697
Christian <christian.albrandaguirre@cesunbc.edu.mx>
2024-11-01 23:59:23 -0700

    add more style to webpage and display tasks

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,54 @@
-function hello(){
-    console.log("hello there");
+// function hello(){
+//     console.log("hello there");
+// }
+
+// function main(){
+//     hello();
+//     console.log("Hello im the main");
+// }
+
+// window.onload = main;
+
+function saveTask(){
+    console.log("save task fn")
+
+    // get the values
+    const title = $("#txtTitle").val();
+    const desc = $("#txtDescription").val();
+    const color = $("#selColor").val();
+    const date = $("#selDate").val();
+    const status = $("#selStatus").val();
+    const budget = $("#numBudget").val();
+    let taskToSave = new task(title, desc, color, date, status, budget)
+    console.log(taskToSave)
+    displayTask(taskToSave);
 }
 
-function main(){
-    hello();
-    console.log("Hello im the main");
+function displayTask(taskToSave){
+    let syntax = `
+        <div class="task-container" style = "border-color:${taskToSave.color}">
+            <div class="task">
+                <div class="info">
+                    <h5> ${taskToSave.title} </h5>
+                    <p>  ${taskToSave.description} </p>
+                </div>
+
+                <div class="status">${taskToSave.status}</div>
+
+                <div class="date-budget">
+                    <span>${taskToSave.date}</span>
+                    <span>${taskToSave.budget}</span>
+                </div>
+            </div>
+        </div>
+    `
+
+    $("#list").append(syntax)
 }
 
-window.onload = main;
+function init(){
+    console.log("init");
+    $("#btnSave").click(saveTask)
+}
+
+window.onload = init;
---
+++ scripts/task.js
@@ -0,0 +1,12 @@
+class task {
+    constructor(title, desc, color, date, status, budget){
+        this.title = title;
+        this.description = desc;
+        this.color = color;
+        this.date = date;
+        this.status = status;
+        this.budget = budget;
+        //this.name = "Christian"
+    }
+
+}
